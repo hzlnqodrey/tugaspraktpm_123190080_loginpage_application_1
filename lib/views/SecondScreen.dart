b5248962892b78d983450554d9d4c921a182f67b
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2024-03-14 14:04:08 +0700

    feat: add Halaman List Picture and Name with model tourism place to passing data

--- lib/views/SecondScreen.dart
+++ lib/views/SecondScreen.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
+import '../modul/tourism_place.dart';
 
 class SecondScreen extends StatelessWidget {
   SecondScreen({super.key}) {}
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return Card(
-          child: Padding(
-              padding: const EdgeInsets.all(2), child: Text('Text $index')),
-        );
-      },
-      itemCount: 100,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Halaman List'),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final TourismPlace place = tourismPlaceList[index];
+          return InkWell(
+              onTap: () {},
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: 100,
+                    height: 100,
+                    child: Image.network(place.imageUrls[0]),
+                  ),
+                  Text(place.name),
+                ],
+              ));
+        },
+        itemCount: tourismPlaceList.length,
+      ),
     );
   }
 }
